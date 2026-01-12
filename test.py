341cf611074f4347bcb8b5538bb5a2bfa256c7ed
Vasanter <sco_antares@bk.ru>
2026-01-12 22:34:55 +0300

    add update v0.3.6

--- test.py
+++ test.py
@@ -1,7 +1,2 @@
-print("Таблица умножения:")
-print("-" * 155)
-
-for i in range(1, 11):
-    for j in range(1, 11):
-        print(f"{i} * {j} = {i * j}", end="    \t")
-    print()  # переход на новую строку
+import random
+import pytest
