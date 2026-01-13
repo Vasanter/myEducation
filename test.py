5a207121e233814f7ddcfe5fa1f2a33e98e100b4
Vasanter <sco_antares@bk.ru>
2026-01-13 21:48:37 +0300

    add update v0.3.7

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
-import random
-import pytest
+x = 10
+x = 20
+
+a, b = 1, 2.5
+
+print(a + b)  # Сложение
+print(a - b)  # Вычитание
+print(a * b)  # Умножение
+print(a / b)  # Деление
+print(a ** b)  # Возведение a в степень
+print(a % b)  # Остаток от деления
+
+print(a == b)  # равны ли a и b
+print(a != b)  # не равны ли a и b
+print(a > b)  # больше ли a чем b
+print(a < b)  # меньше ли a чем b
+print(a >= b)  # больше или равно ли a чем b
+print(a <= b)  # меньше или равно ли a чем b
